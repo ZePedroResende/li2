f4567c4f1b10af1c661bc5903492aeceee9f0c8a
ZePedroResende <j.p.resende68@gmail.com>
2016-05-02 01:34:46 +0100

    tentativa de organizaçao

--- src/manipulaMao/base.c
+++ src/manipulaMao/base.c
@@ -4,23 +4,6 @@
 #include <time.h>
 #include "big2.h"
 
-/* Transforma um estado numa string do url, e retorna essa string. */
-char* estado2str(ESTADO e){
-    static char str[10240];
-    sprintf(str, FORMATO, e.mao[0], e.mao[1], e.mao[2], e.mao[3], e.highlight, e.cartas[0], e.cartas[1], e.cartas[2], e.cartas[3], e.play, e.pass, e.card,e.ultimo_jogador, e.ultima_jogada, e.actual_jogador, e.cartas_bots[0], e.cartas_bots[1], e.cartas_bots[2], e.cartas_bots[3], e.layout,e.start);
-    
-    return str;
-}
-
-/* Transforma a string do url num estado novo, e retorna esse estado. */
-ESTADO str2estado(char* str){
-    ESTADO e;
-    sscanf(str, FORMATO, &e.mao[0], &e.mao[1], &e.mao[2], &e.mao[3], &e.highlight, &e.cartas[0], &e.cartas[1], &e.cartas[2], &e.cartas[3], &e.play, &e.pass, &e.card,&e.ultimo_jogador, &e.ultima_jogada, &e.actual_jogador, &e.cartas_bots[0], &e.cartas_bots[1], &e.cartas_bots[2], &e.cartas_bots[3], &e.layout,&e.start);
-    
-    return e;
-}
-
-
 /** brief Devolve o índice da carta
 @param naipe O naipe da carta (inteiro entre 0 e 3)
 @param valor O valor da carta (inteiro entre 0 e 12)
