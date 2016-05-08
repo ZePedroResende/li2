723c34c54cdb9777debfd521161e5ce6cec9ef34
ZePedroResende <j.p.resende68@gmail.com>
2016-05-08 19:53:36 +0100

    inverter a ordem das jogadas

--- src/geraHTML.c
+++ src/geraHTML.c
@@ -23,20 +23,20 @@ void imprime_carta(char *path, int x, int y, ESTADO e, int mao, int naipe, int v
     char script[10240];
     ESTADO novo = e;
     novo.card = 1;
-    
+
     if (mao == 0 && e.actual_jogador == 0) {
         if (carta_existe (novo.highlight, naipe, valor)) {
             novo.highlight = rem_carta(novo.highlight, naipe, valor);
         }
-        
+
         else {
             novo.highlight = add_carta(novo.highlight, naipe, valor);
         }
-        
+
         sprintf(script, "%s?%s", SCRIPT, estado2str(novo));
-        printf("<a xlink:href = \"%s\"><image x = \"%d\" y = \"%d\" height = \"110\" width = \"80\" xlink:href = \"%s/%c%c.svg\" /></a>\n", script, x, y, path, rank[valor], suit[naipe]);  
+        printf("<a xlink:href = \"%s\"><image x = \"%d\" y = \"%d\" height = \"110\" width = \"80\" xlink:href = \"%s/%c%c.svg\" /></a>\n", script, x, y, path, rank[valor], suit[naipe]);
     }
-    
+
     else {
         if (carta_existe(e.mao[1],naipe,valor) || carta_existe(e.mao[2],naipe,valor) || carta_existe(e.mao[3],naipe,valor))
             printf("<image x = \"%d\" y = \"%d\" height = \"110\" width = \"80\" xlink:href = \"http://localhost/imagens/11C.png\" />\n", x, y);
@@ -48,7 +48,7 @@ void imprime_carta(char *path, int x, int y, ESTADO e, int mao, int naipe, int v
 Função auxiliar que recebe parametros da função imprime. Aqui é vista a forma de como as cartas são apresentadas, neste caso por valor.
 */
 void imprime_aux_valor (ESTADO e, int m, int bx1, int by1, int bx2, int by2, int bx3, int by3, int x, int y, char *path) {
-    
+
     int n,v;
 
     for (v = 0; v < 13; v++) {
@@ -128,11 +128,11 @@ Apresenta as cartas no tabuleiro nas respetivas coordenadas.
 @param e O estado do jogo actual.
 */
 void imprime (char *path, ESTADO e) {
-    
-    int m, bx1= 820 , by1 = 250 , bx2=570 , by2 = 150 , bx3= 395 , by3 = 250;
-    int X[4] = {405, 1190, 405, 10};
+
+    int m, bx1= 395  , by1 = 250 , bx2=570 , by2 = 150 , bx3= 820 , by3 = 250;
+    int X[4] = {405, 10 , 405, 1190};
     int Y[4] = {595, 100, 15, 100};
-    
+
     for(m = 0; m < 4; m++) {
         int x = X[m], y = Y[m];
         if (e.layout == 1) {
@@ -144,16 +144,16 @@ void imprime (char *path, ESTADO e) {
     }
 }
 
-/** 
+/**
 Esta função trata de imprimir o botão "SUBMIT" na página HTML. Aqui, usamos a função posso_jogar.
-Se for possível jogar, o botão é clicável, sendo primitida a jogada. Caso contrário, o botão fica mais escuro, não sendo clicável. 
+Se for possível jogar, o botão é clicável, sendo primitida a jogada. Caso contrário, o botão fica mais escuro, não sendo clicável.
 @param e O estado actual.
 */
 void imprime_botao_jogar(ESTADO e) {
-    
+
     char script[10240];
     ESTADO novo = e;
-    
+
     if (posso_jogar(e) && e.highlight != 0) {
         novo.ultima_jogada = e.highlight;
         novo.cartas[0] = e.cartas[0] - (numero_de_cartas(novo.ultima_jogada));
@@ -161,34 +161,34 @@ void imprime_botao_jogar(ESTADO e) {
         novo.play = 1;
         novo.ultimo_jogador = 0;
         sprintf(script, "%s?%s", SCRIPT, estado2str(novo));
-        printf("<a xlink:href = \"%s\"><image x = \"510\" y = \"710\" height = \"80\" width = \"80\" xlink:href = \"http://localhost/imagens/SubmitLI2.png\" /></a>\n", script); 
+        printf("<a xlink:href = \"%s\"><image x = \"510\" y = \"710\" height = \"80\" width = \"80\" xlink:href = \"http://localhost/imagens/SubmitLI2.png\" /></a>\n", script);
     }
-    
+
     else {
-        printf("<image x = \"510\" y = \"710\" height = \"80\" width = \"80\" xlink:href = \"http://localhost/imagens/SubmitLI2out.png\" />\n"); 
+        printf("<image x = \"510\" y = \"710\" height = \"80\" width = \"80\" xlink:href = \"http://localhost/imagens/SubmitLI2out.png\" />\n");
     }
 }
 
-/** 
+/**
 Esta função trata de imprimir o botão "PASS" na página HTML. Quando o utilizador não tem nenhuma combinação válida possível
 em relação a última jogada, é possível usar o botão "PASS". O utilizador pode usar sempre o botão "PASS",
-excepto quando é o primeiro a jogar (3 de ouros) ou quando todos passam a jogada e este foi o ultimo a jogar uma carta/combinaçao 
+excepto quando é o primeiro a jogar (3 de ouros) ou quando todos passam a jogada e este foi o ultimo a jogar uma carta/combinaçao
 @param e O estado actual.
 */
 void imprime_botao_passar(ESTADO e) {
-    
+
     char script[10240];
     ESTADO novo = e;
-    
+
     if (e.actual_jogador == 0 && e.ultima_jogada != -1 && e.ultimo_jogador != 0) {
         novo.ultima_jogada = e.ultima_jogada;
         novo.highlight = 0;
         novo.actual_jogador = incrementa_jogador(e);
         novo.pass = 1;
         sprintf(script, "%s?%s", SCRIPT, estado2str(novo));
-        printf("<a xlink:href = \"%s\"><image x = \"610\" y = \"710\" height = \"80\" width = \"80\" xlink:href = \"http://localhost/imagens/PassLI2.png\" /></a>\n", script); 
+        printf("<a xlink:href = \"%s\"><image x = \"610\" y = \"710\" height = \"80\" width = \"80\" xlink:href = \"http://localhost/imagens/PassLI2.png\" /></a>\n", script);
     }
-    
+
     else {
         printf("<image x = \"610\" y = \"710\" height = \"80\" width = \"80\" xlink:href = \"http://localhost/imagens/PassLI2out.png\" />\n");
     }
@@ -201,9 +201,9 @@ A função imprime_botao_clear imprime o botão clear para o browser, o qual vai
 @param e O estado actual.
 */
 void imprime_botao_clear(ESTADO e) {
-    
+
     char script[10240];
-    
+
     e = clear(e);
     sprintf(script, "%s?%s", SCRIPT, estado2str(e));
     printf("<a xlink:href = \"%s\"><image x = \"710\" y = \"710\" height = \"80\" width = \"80\" xlink:href = \"http://localhost/imagens/ClearLI2.png\" /></a>\n", script);
@@ -214,16 +214,16 @@ A função imprime_botao_sugestao imprime o botão de sugestão para o browser.
 @param e O estado actual.
 */
 void imprime_botao_sugestao(ESTADO e) {
-    
+
     char script[10240];
     e = clear(e);
-   
+
     e = sugestao(e);
- 
-    if (e.highlight == 0){ 
+
+    if (e.highlight == 0){
         e = passar(e);
 
-}   
+}
     sprintf(script, "%s?%s", SCRIPT, estado2str(e));
     printf("<a xlink:href = \"%s\"><image x = \"40\" y = \"710\" height = \"80\" width = \"80\" xlink:href = \"http://localhost/imagens/sugestaostallman.gif\" /></a>\n", script);
 }
@@ -234,16 +234,16 @@ A função imprime_botao_start imprime o botão de start do menu inicial para o 
 @param e O estado actual.
 */
 void imprime_botao_start(ESTADO e) {
-    
+
     char script[10240];
-    
+
     if (e.start == 1){
         e = bots1(baralhar());
-        
+
         while(e.actual_jogador != 0){
             e = bots2(e);
         }
-        
+
         e.start=0;
         sprintf(script, "%s?%s", SCRIPT, estado2str(e));
         printf("<rect x = \"0\" y = \"0\" height = \"800\" width = \"1280\" style = \"fill:#000000\"/>\n");
@@ -257,19 +257,19 @@ A função imprime_botao_exit imprime o botão de exit para o browser, de forma 
 @param e O estado actual.
 */
 void imprime_botao_exit(ESTADO e) {
-    
+
     char script[10240];
-    
+
     if (e.start == 0){
         e = bots1(baralhar());
-        
+
         while(e.actual_jogador != 0){
             e = bots2(e);
         }
-        
+
         e.start=1;
         sprintf(script, "%s?%s", SCRIPT, estado2str(e));
-        printf("<a xlink:href = \"%s\"><image x = \"1140\" y = \"603\" height = \"80\" width = \"100\" xlink:href = \"http://localhost/imagens/ExitLI2.png\" /></a>\n", script);   
+        printf("<a xlink:href = \"%s\"><image x = \"1140\" y = \"603\" height = \"80\" width = \"100\" xlink:href = \"http://localhost/imagens/ExitLI2.png\" /></a>\n", script);
     }
 }
 
@@ -279,15 +279,15 @@ A função imprime_botao_reset imprime um botão que quando algum dos jogadores 
 @param e O estado actual.
 */
 void imprime_botao_reset(ESTADO e) {
-    
+
     char script[10240];
-    
+
     if (e.cartas[0] != 0 && e.cartas[1] != 0 && e.cartas[2] != 0 && e.cartas[3] != 0) {
-        
+
     }
     else {
         e = bots1(baralhar());
-        
+
         while(e.actual_jogador != 0) {
             e = bots2(e);
         }
@@ -300,18 +300,18 @@ void imprime_botao_reset(ESTADO e) {
 /*
 A função imprime_botao_layout imprime um botão que muda a organização inicial das cartas (de naipe para valor).
 @param e O estado actual.
-*/ 
+*/
 void imprime_botao_layout(ESTADO e) {
-    
+
     char script[10240];
-    
+
     if (e.layout == 0) {
         e.layout = 1;
     }
     else {
         e.layout = 0;
     }
-    
+
     sprintf(script, "%s?%s", SCRIPT, estado2str(e));
     printf("<a xlink:href = \"%s\"><image x = \"1140\" y = \"710\" height = \"80\" width = \"80\" xlink:href = \"http://localhost/imagens/Reset.png\" /></a>\n", script);
 }
@@ -334,10 +334,10 @@ void imprime_botao_teste(ESTADO e){
     novo.cartas[1] = numero_de_cartas(novo.mao[1]);
     novo.cartas[2] = numero_de_cartas(novo.mao[2]);
     novo.cartas[3] = numero_de_cartas(novo.mao[3]);
-  
+
 
         e = bots1(novo);
-        
+
         while(e.actual_jogador != 0){
             e = bots2(e);
         }
@@ -352,28 +352,28 @@ A função imprime_botao_trofeu imprime uma imagem com um trofeu correspondente 
 @param e O estado actual.
 */
 void imprime_botao_trofeu(ESTADO e) {
-    
+
     char script[10240];
-    
+
     if (e.cartas[0] == 0) {
         sprintf(script, "%s?%s", SCRIPT, estado2str(e));
         printf("<a xlink:href = \"%s\"><image x = \"1200\" y = \"40\" height = \"750\" width = \"750\" xlink:href = \"http://localhost/imagens/trofeu.png\" /></a>\n", script);
     }
-    
+
     if (e.cartas[1] == 0) {
         sprintf(script, "%s?%s", SCRIPT, estado2str(e));
         printf("<a xlink:href = \"%s\"><image x = \"1200\" y = \"40\" height = \"750\" width = \"750\" xlink:href = \"http://localhost/imagens/trofeu2.png\" /></a>\n", script);
     }
-    
+
     if (e.cartas[2] == 0) {
         sprintf(script, "%s?%s", SCRIPT, estado2str(e));
         printf("<a xlink:href = \"%s\"><image x = \"1200\" y = \"40\" height = \"750\" width = \"750\" xlink:href = \"http://localhost/imagens/trofeu3.png\" /></a>\n", script);
     }
-    
+
     if (e.cartas[3] == 0){
         sprintf(script, "%s?%s", SCRIPT, estado2str(e));
         printf("<a xlink:href = \"%s\"><image x = \"1200\" y = \"40\" height = \"750\" width = \"750\" xlink:href = \"http://localhost/imagens/trofeu4.png\" /></a>\n", script);
-    } 
+    }
 }
 
 /** brief Trata os argumentos da CGI
@@ -385,38 +385,38 @@ Caso não seja passado nada à cgi-bin, ela assume que todas as cartas estão pr
 */
 
 void parse (char *query) {
-    
+
     ESTADO e;
-    
+
     int a;
-    
+
     if (query != NULL && strlen(query) != 0) {
-       
+
         e = str2estado(query);
-        
+
         if (e.card) e.card = 0;
         if (e.play) e = jogar(e);
-        if (e.pass) e = passar(e); 
+        if (e.pass) e = passar(e);
     }
-    
+
     else {
-    
+
         e = bots1(baralhar());
-        
+
         while(e.actual_jogador != 0){
             e = bots2(e);
         }
-        
+
         a=e.mao[0];
         printf("%dn", a);
     }
-    
+
     if (e.start == 1) {
         imprime_botao_start(e);
     }
 
     else{
-        
+
         if (e.cartas[0] == 0 || e.cartas[1] == 0 || e.cartas[2] == 0 || e.cartas[3] == 0) {
             imprime_botao_trofeu(e);
             imprime(BARALHO,e);
@@ -431,7 +431,7 @@ void parse (char *query) {
             imprime_botao_clear(e);
             imprime_botao_sugestao(e);
             imprime_botao_exit(e);
-            imprime_botao_teste(e); 
+            imprime_botao_teste(e);
         }
     }
 }
